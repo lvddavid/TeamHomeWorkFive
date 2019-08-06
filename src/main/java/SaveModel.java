0452d229312d28a5d53047b95c6ffe43b158b209
Yuriy_V <45740455+Yuriy4597@users.noreply.github.com>
2019-08-06 16:34:40 +0300

    Release

---
+++ src/main/java/SaveModel.java
@@ -0,0 +1,60 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class SaveModel {
+
+        @JsonProperty("NameOfArtist")
+        private String NameOfArtist;
+
+        @JsonProperty("SongName")
+        private String SongName;
+
+        @JsonProperty("Minutes")
+        private int Minutes;
+
+
+        public String getNameOfArtist() {
+            return NameOfArtist;
+        }
+
+        public void setNameOfArtist(String nameOfArtist) {
+            NameOfArtist = nameOfArtist;
+        }
+
+        public String getSongName() {
+            return SongName;
+        }
+
+        public void setSongName(String songName) {
+            SongName = songName;
+        }
+
+        public int getMinutes() {
+            return Minutes;
+        }
+
+        public void setMinutes(int minutes) {
+            Minutes = minutes;
+        }
+
+        @Override
+        public String toString() {
+            return "SaveModel{" +
+                    "NameOfArtist='" + NameOfArtist + '\'' +
+                    ", SongName='" + SongName + '\'' +
+                    ", Minutes=" + Minutes +
+                    '}';
+        }
+
+
+
+
+
+
+}
---
+++ src/main/java/UserModelList.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+
+public class UserModelList {
+
+    @Override
+    public String toString() {
+        return "UserModelList{" +
+                "Album=" + Album +
+                '}';
+    }
+
+    private List<SaveModel> Album;
+    public List<SaveModel> getAlbum() {
+        return Album;
+    }
+
+    public void setAlbum(List<SaveModel> album) {
+        this.Album = album;
+    }
+}
