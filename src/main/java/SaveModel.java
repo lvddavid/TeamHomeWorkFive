4e92b14e1b17e5b06a9534c6e0b6c8e386e061bd
Yuriy_V <45740455+Yuriy4597@users.noreply.github.com>
2019-08-07 14:09:31 +0300

    Update SaveModel.java

--- src/main/java/SaveModel.java
+++ src/main/java/SaveModel.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
-public class SaveModel {
+public  class SaveModel {
 
         @JsonProperty("NameOfArtist")
         private String NameOfArtist;
@@ -19,42 +19,37 @@ public class SaveModel {
         private int Minutes;
 
 
-        public String getNameOfArtist() {
-            return NameOfArtist;
-        }
+    public String getNameOfArtist() {
+        return NameOfArtist;
+    }
 
-        public void setNameOfArtist(String nameOfArtist) {
-            NameOfArtist = nameOfArtist;
-        }
+    public void setNameOfArtist(String nameOfArtist) {
+        this.NameOfArtist = nameOfArtist;
+    }
 
-        public String getSongName() {
-            return SongName;
-        }
+    public String getSongName() {
+        return SongName;
+    }
 
-        public void setSongName(String songName) {
-            SongName = songName;
-        }
+    public void setSongName(String songName) {
+        this.SongName = songName;
+    }
 
-        public int getMinutes() {
-            return Minutes;
-        }
+    public int getMinutes() {
+        return Minutes;
+    }
 
-        public void setMinutes(int minutes) {
-            Minutes = minutes;
-        }
-
-        @Override
-        public String toString() {
-            return "SaveModel{" +
-                    "NameOfArtist='" + NameOfArtist + '\'' +
-                    ", SongName='" + SongName + '\'' +
-                    ", Minutes=" + Minutes +
-                    '}';
-        }
+    public void setMinutes(int minutes) {
+        this.Minutes = minutes;
+    }
 
 
-
-
-
-
+    @Override
+    public String toString() {
+        return "SaveModel{" +
+                "NameOfArtist=  '" + NameOfArtist + '\'' +
+                ", SongName=  '" + SongName + '\'' +
+                ", Minutes=  " + Minutes +
+                '}';
+    }
 }
